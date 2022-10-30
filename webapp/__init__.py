4aa6695fb3f19068be584432a923d78764f73866
ZagorenkoV <vzagorenko@bk.ru>
2022-10-30 21:00:11 +0300

    Добавил код в all_in, добавил работающие кнопки

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, send_from_directory
 
 from webapp.mobile.models import db as db1
 from webapp.all_in.models import db as db2
-from webapp.mobile.queries import queries
+from webapp.mobile.queries import queries as queries1
+from webapp.all_in.queries import queries as queries2
 
 
 def create_app():
@@ -16,7 +17,7 @@ def create_app():
         request_data = request.args
         title = "Сравнение мобильных операторов"
         if request_data:
-            tarifs_list = queries(request_data)
+            tarifs_list = queries1(request_data)
             return render_template('mobile/mobile.html', title=title, tarifs_list=tarifs_list, tarifs_list_len=len(tarifs_list))
         else:
             return render_template('mobile/mobile.html', title=title)
@@ -26,5 +27,15 @@ def create_app():
         return send_from_directory(
         app.config['UPLOAD_FOLDER'], name, as_attachment=True
     )
+
+    @app.route("/all_in")
+    def index1():
+        request_data = request.args
+        title = "Сравнение мобильных операторов"
+        if request_data:
+            tarifs_list = queries2(request_data)
+            return render_template('all_in/all_in.html', title=title, tarifs_list=tarifs_list, tarifs_list_len=len(tarifs_list))
+        else:
+            return render_template('all_in/all_in.html', title=title)
 
     return app
