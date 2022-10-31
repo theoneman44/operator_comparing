788d364506091de49413c487c994386467aaf9c3
Molin Simon <theoneman44@gmail.com>
2022-10-31 23:38:02 +0300

    Отключение фильтра по смс если нет результата

--- webapp/mobile/queries.py
+++ webapp/mobile/queries.py
@@ -12,23 +12,36 @@ def queries(request_result):
                                      int(request_result['phone_sms_quantity']) + 50
                                      ))
 
-    if request_result['mobile_operator_name'] != '0':
-        tarifs_list = tarifs_list.filter(Tarif.mobile_operator_name == request_result['mobile_operator_name'])
+    def standart_queries(request_result, tarifs_list):
+        if request_result['mobile_operator_name'] != '0':
+            tarifs_list = tarifs_list.filter(Tarif.mobile_operator_name == request_result['mobile_operator_name'])
 
-    if 'unlim_phone_internet' not in request_result.keys():
-        tarifs_list = tarifs_list.filter(between(Tarif.phone_internet_quantity, int(request_result['phone_internet_quantity']) - 5,
-                                         int(request_result['phone_internet_quantity']) + 5
-                                         ))
-    elif request_result['unlim_phone_internet'] == '1':
-        tarifs_list = tarifs_list.filter(Tarif.unlim_phone_internet == request_result['unlim_phone_internet'])
+        if 'unlim_phone_internet' not in request_result.keys():
+            tarifs_list = tarifs_list.filter(between(Tarif.phone_internet_quantity, int(request_result['phone_internet_quantity']) - 5,
+                                             int(request_result['phone_internet_quantity']) + 5
+                                             ))
+        elif request_result['unlim_phone_internet'] == '1':
+            tarifs_list = tarifs_list.filter(Tarif.unlim_phone_internet == request_result['unlim_phone_internet'])
 
-    if 'unlim_phone_minutes' not in request_result.keys():
-        tarifs_list = tarifs_list.filter(between(Tarif.phone_minutes_quantity, int(request_result['phone_minutes_quantity']) - 199,
-                                         int(request_result['phone_minutes_quantity']) + 199
-                                         ))
-    elif request_result['unlim_phone_minutes'] == '1':
-        tarifs_list = tarifs_list.filter(Tarif.unlim_phone_minutes == request_result['unlim_phone_minutes'])
+        if 'unlim_phone_minutes' not in request_result.keys():
+            tarifs_list = tarifs_list.filter(between(Tarif.phone_minutes_quantity, int(request_result['phone_minutes_quantity']) - 199,
+                                             int(request_result['phone_minutes_quantity']) + 199
+                                             ))
+        elif request_result['unlim_phone_minutes'] == '1':
+            tarifs_list = tarifs_list.filter(Tarif.unlim_phone_minutes == request_result['unlim_phone_minutes'])
 
-    tarifs_list = tarifs_list.order_by(Tarif.price.asc()).all()
+        tarifs_list = tarifs_list.order_by(Tarif.price.asc()).all()
+        return tarifs_list
 
-    return tarifs_list
+    def queries_without_sms(request_result):
+        tarifs_list = Tarif.query
+        tarifs_list = standart_queries(request_result, tarifs_list)
+        return tarifs_list
+
+    tarifs_list = standart_queries(request_result, tarifs_list)
+
+    if len(tarifs_list) == 0:
+        tarifs_list = queries_without_sms(request_result)
+        return tarifs_list
+    else:
+        return tarifs_list
