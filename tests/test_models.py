15c76134d114c1afecf433c6ab5b08ba97305344
Molin Simon <theoneman44@gmail.com>
2024-01-26 22:51:48 +0300

    changing tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,28 +1,16 @@
 from webapp.mobile.models import Links, Tarif, Tarif_3in1
 
 
-def test__model__links() -> bool:
-    link = Links()
-    link.mobile_operator_name = 'МТС'
-    link.tarif_name = 'НеТариф'
-    link.page_link = 'https://moskva.mts.ru/personal/mobilnaya-svyaz/tarifi/vse-tarifi/mobile'
-
+def test__model__links():
+    link = Links(mobile_operator_name='МТС', tarif_name='НеТариф', page_link='https://moskva.mts.ru/personal/mobilnaya-svyaz/tarifi/vse-tarifi/mobile')
     assert link.__repr__() == 'Tarif МТС, НеТариф, https://moskva.mts.ru/personal/mobilnaya-svyaz/tarifi/vse-tarifi/mobile.'
 
 
-def test__model__tarif() -> bool:
-    tarif = Tarif()
-    tarif.mobile_operator_name = 'МТС'
-    tarif.tarif_name = 'НеТариф'
-    tarif.price = 599
-
+def test__model__tarif():
+    tarif = Tarif(mobile_operator_name='МТС', tarif_name='НеТариф', price=599)
     assert tarif.__repr__() == 'Tarif МТС, НеТариф, 599'
 
 
-def test__model__tarif_3in1() -> bool:
-    tarif = Tarif_3in1()
-    tarif.mobile_operator_name = 'Теле2'
-    tarif.tarif_name = 'Black'
-    tarif.price = 800
-
+def test__model__tarif_3in1():
+    tarif = Tarif_3in1(mobile_operator_name='Теле2', tarif_name='Black', price=800)
     assert tarif.__repr__() == 'Tarif Теле2, Black, 800'
--- tests/test_response.py
+++ tests/test_response.py
@@ -2,14 +2,14 @@ import pytest
 from flask import Flask
 
 
-@pytest.mark.parametrize('url, response_code',
-                         [('/images/megafon_banner.jpg', 200),
-                          ('/images/mts_banner.jpg', 200),
-                          ('/', 200),
-                          ('/all_in', 200),
-                          ('/?mobile_operator_name=МТС&phone_internet_quantity=30&phone_minutes_quantity=1000&phone_sms_quantity=250', 200),
-                          ('/all_in?mobile_operator_name=0&phone_internet_qty=30&phone_minutes_qty=1000&internet_speed=400&channels_qty=200', 200)
+@pytest.mark.parametrize('url',
+                         [('/images/megafon_banner.jpg'),
+                          ('/images/mts_banner.jpg'),
+                          ('/'),
+                          ('/all_in'),
+                          ('/?mobile_operator_name=МТС&phone_internet_quantity=30&phone_minutes_quantity=1000&phone_sms_quantity=250'),
+                          ('/all_in?mobile_operator_name=0&phone_internet_qty=30&phone_minutes_qty=1000&internet_speed=400&channels_qty=200')
                           ])
-def test__status_codes(client: Flask, url: str, response_code: int) -> bool:
+def test__status_codes(client: Flask, url: str):
     response = client.get(url)
-    assert response.status_code == response_code
+    assert response.status_code == 200
